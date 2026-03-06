d5cdf092961afd48c43817f59b1abb0e5aa0c0fa
helenalyq1023-cmyk <helenalyq1023@gmail.com>
2026-03-06 15:50:38 +0800

    Delete Habit tracker directory

--- Habit tracker/test.py
+++
@@ -1,8 +0,0 @@
-import certifi
-from pymongo import MongoClient
-
-uri = "mongodb+srv://helenalyq1023_db_user:Uj28BgryGB0K7WIW@habittracker.xmvpoxl.mongodb.net/?appName=HabitTracker"
-
-client = MongoClient(uri, tlsCAFile=certifi.where())
-
-print(client.list_database_names())
